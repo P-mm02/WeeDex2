31f3223c124e367038525750f9fe956b1a4b3ed8
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-15 23:19:50 +0700

    add login mm

---
+++ views/login.js
@@ -0,0 +1,18 @@
+
+document.getElementById('LoginButton').addEventListener('click', event => {
+    let account
+    let btn = event.target
+    
+    ethereum.request({method: 'eth_requestAccounts'}).then(accounts => {
+      account = accounts[0]
+      console.log(account)
+      btn.textContent = account
+      document.getElementById("LoginButton").style.fontSize = '0.75vw'
+      ethereum.request({method: 'eth_getBalance' , params: [account, 'latest']}).then(result => {
+        console.log(result)
+        let wei = parseInt(result,4);
+        let balance = wei / (10**18)
+        console.log(balance + " ETH")
+      })
+    })
+  })
