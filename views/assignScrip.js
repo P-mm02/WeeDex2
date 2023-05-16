4f08c6c91dc518adf8540bb5402eb3b49c665862
P-mm02 <poomtawee.mm02@gmail.com>
2023-05-16 09:56:48 +0700

    fin

---
+++ views/assignScrip.js
@@ -0,0 +1,15 @@
+function listAdress1(){
+    document.getElementById("listAdress55").value=document.getElementById("listAdress1").innerHTML    
+}
+function listAdress2(){
+    document.getElementById("listAdress55").value=document.getElementById("listAdress2").innerHTML    
+}
+function listAdress3(){
+    document.getElementById("listAdress55").value=document.getElementById("listAdress3").innerHTML    
+}
+function listAdress4(){
+    document.getElementById("listAdress55").value=document.getElementById("listAdress4").innerHTML    
+}
+function listAdress5(){
+    document.getElementById("listAdress55").value=document.getElementById("listAdress5").innerHTML    
+}
